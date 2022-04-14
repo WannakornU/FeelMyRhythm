0c32f30a6db5b0da07a5e8600f9efb2968ab8bf0
WannakornU <jam0924914626@gmail.com>
2022-04-14 13:45:15 +0700

    init

---
+++ src/FeelMyRhythm.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rada
+ */
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+import javax.swing.JFrame;
+public class FeelMyRhythm extends JFrame{
+    
+    private Image screenImage;
+    private Graphics screenGraphic;
+    
+    private Image introBackground;
+    
+    public FeelMyRhythm() {
+        setTitle("FEEL MY RHYTHM");
+        setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+        introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.png")).getImage();
+    }
+
+    public void paint(Graphics g) {
+        screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+        screenGraphic = screenImage.getGraphics();
+        screenDraw(screenGraphic);
+        g.drawImage(screenImage, 0, 0, null);
+    }
+    
+    public void screenDraw(Graphics g) {
+        g.drawImage(introBackground, 0, 0, null);
+        this.repaint();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author rada
+ */
+public class Main {
+
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new FeelMyRhythm();
+    }
+    
+}
