5065a482a681083e3a89f0f3aa75d4dc5cbd1934
WannakornU <jam0924914626@gmail.com>
2022-04-18 17:59:08 +0700

    add startbutton

--- src/FeelMyRhythm.java
+++ src/FeelMyRhythm.java
@@ -10,6 +10,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -20,9 +22,18 @@ public class FeelMyRhythm extends JFrame{
     private Image screenImage;
     private Graphics screenGraphic;
     
-    private Image introBackground = new ImageIcon(Main.class.getResource("/images/introBackground.jpg")).getImage();
-    private JButton startButton = new JButton(new ImageIcon(Main.class.getResource("/images/startButtonBasic.png")));
+    private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("/images/startButtonBasic.png"));
+    private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("/images/startButtonEntered.png"));
     
+    private Image introBackground = new ImageIcon(Main.class.getResource("/images/introBackground.jpg")).getImage();
+    
+    
+    private JButton startButton = new JButton(startButtonBasicImage);
+    
+    
+   
+    
+   
     public FeelMyRhythm() {
         //setUndecorated(true);
         setTitle("FEEL MY RHYTHM");
@@ -32,11 +43,31 @@ public class FeelMyRhythm extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         //setBackground(new Color(0, 0, 0, 0));
-        //setLayout(null);
+        setLayout(null);
        
-        
-        startButton.setBounds(50, 50, 30, 30);
+        startButton.setBounds(400, 50, 500, 150);
+        startButton.setBorderPainted(false);
+        startButton.setContentAreaFilled(false);
+        startButton.setFocusPainted(false);
+        startButton.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent e){
+                startButton.setIcon(startButtonEnteredImage);
+            }
+            @Override
+            public void mouseExited(MouseEvent e){
+                startButton.setIcon(startButtonBasicImage);
+            }
+            @Override
+            public void mousePressed(MouseEvent e){
+                Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3",false);
+                buttonEnteredMusic.start();
+            }
+            
+        });
         add(startButton);
+        
+        
         
         
         Music introMusic = new Music("introMusic.mp3",true);
@@ -52,7 +83,7 @@ public class FeelMyRhythm extends JFrame{
     
     public void screenDraw(Graphics g) {
         g.drawImage(introBackground, 0, 0, null);
-        paintComponents(g);
+        //paintComponents(g);
         this.repaint();
     }
 
